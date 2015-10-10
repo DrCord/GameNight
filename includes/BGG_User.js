b4fefa12a8fca22cfb917e9a57070df0e299bc8a
Cord Slatton-Valle <cord42@gmail.com>
2015-10-10 13:46:51 -0700

    adjust bgg api call methods to return data deeper in teh structure

--- includes/BGG_User.js
+++ includes/BGG_User.js
@@ -9,13 +9,18 @@ var BGG_User = function(username){
 };
 // Make our prototype from User.prototype so we inherit User's methods
 BGG_User.prototype = Object.create(User.prototype);
-// Modify functions
+// Modify functions from User constructor
 BGG_User.prototype.getUserCollection = function() {
     console.log('BGG_User.getUserCollection() called');
     var thisUser = this;
     return bgg('collection', {username: thisUser.username})
         .then(function(results){
-            thisUser.collection = results;
+            if(typeof results['items']['item'] != "undefined"){
+                thisUser.collection = results['items']['item'];
+            }
+            else{
+                thisUser.collection = {};
+            }
         }).then(function(){
             return thisUser;
         })
@@ -26,13 +31,24 @@ BGG_User.prototype.getUserData = function() {
     var thisUser = this;
     return bgg('user', {name: thisUser.username})
         .then(function(results){
-            thisUser.userData = results;
+            thisUser.userData = results['user'];
         }).then(function(){
             return thisUser;
         })
     ;
 };
+BGG_User.prototype.update = function() {
+    console.log('BGG_User.update() called');
+    var thisUser = this;
+    return thisUser.getUserData()
+        .then(
+            function(){
+                return thisUser.getUserCollection();
+            }
+        )
+    ;
+};
 // Set BGG_User constructor to itself
 BGG_User.prototype.constructor = BGG_User;
-
+// Export BGG_User
 module.exports = BGG_User;
